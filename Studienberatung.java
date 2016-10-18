be434cdaac6af86446b61150f3b11296c24cbf52
Kevin Hoppe <Kevin Hoppe>
2016-10-18 22:10:05 +0200

    created branch and added question wissbegierig

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,10 +1,14 @@
 import java.util.*;
 public class Studienberatung{
 	public static void main (String[] args){
-		String answerOne;
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Hast du Abitur?");
-		answerOne = scanner.next();
+		String answerOne = scanner.next();
+
+		System.out.println("Bist du wissbegierig?");
+		String answerTwo = scanner.next();
+
+
 	}
 }
