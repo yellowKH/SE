aaf8d07ef4b9b37fb0119798479f8a38b08423d4
Kevin Hoppe <Kevin Hoppe>
2016-10-18 21:35:36 +0200

    implemented question and scanner answer

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,6 +1,10 @@
 import java.util.*;
 public class Studienberatung{
 	public static void main (String[] args){
+		String answerOne;
 		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Hast du Abitur?");
+		answerOne = scanner.next();
 	}
 }
